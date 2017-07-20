2cb253fd8b9def2eb643dd41b7779816ce807f5a
VCBackup <dmoore1525@gmail.com>
2017-07-20 00:15:26 -0600

    Mostly completed assignment.

---
+++ friends/src/Friend.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import './Friend.css';
+
+class Friend extends Component {
+  render(){
+    return (
+        <li className='friend'>
+            <img className="profile-pic" src='http://placebear.com/50/50.jpg' />
+
+                <h3>Cali Fornia</h3>
+
+                <div className="location">
+                    Location: New Port Beach, California, United States
+                </div>
+
+                <div className="status">
+                    Status: I hate the snow. I wish I was on the beach right now!!! <span className="hashtag">#ihateprovo</span>
+                </div>
+
+                <div className="num-friends">
+                    Friends: 1,367
+                </div>
+        </li>
+    );
+  }
+}
+
+export default Friend;
+

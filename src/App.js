ce210ab2caed5ab68d3e14661d7995a9cdc0725b
Moore <DMoore@isolvedhcm.com>
2017-07-20 12:43:51 -0600

    Had issues getting server up and running. It now works.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
     render(){
         return(
             <div>
-                <h1>The<strong>facebook</strong> Friend Machine</h1>
+                <h1>The <strong>facebook</strong> Friend Machine</h1>
                 <div className='friends'>
                 </div>
             </div>
